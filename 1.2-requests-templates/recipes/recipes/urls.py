6cfeb854405a734b5f59f833e2c022a612956340
Hors <iliya.engineer20@gmail.com>
2024-10-09 21:58:48 +0500

    added paths

--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 
 from django.urls import path
 
+from calculator.views import home_view, recipe_view
+
 urlpatterns = [
     # здесь зарегистрируйте вашу view-функцию
+    path('', home_view, name='home'),
+    path('<str:dish>/', recipe_view, name='recipe')
 ]
