6d5a08b50e77256a054729e7cf981bf6dfab60d3
Hors <iliya.engineer20@gmail.com>
2024-10-09 17:24:25 +0500

    test

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,5 +1,8 @@
+import os
+
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+from datetime import datetime
 
 
 def home_view(request):
@@ -8,10 +11,9 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
-    
     # context и параметры render менять не нужно
     # подбробнее о них мы поговорим на следующих лекциях
     context = {
@@ -23,7 +25,7 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
+    current_time = datetime.now().time()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
@@ -32,4 +34,13 @@ def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
     # который возвращает список файлов в рабочей 
     # директории
-    raise NotImplemented
+    items = os.listdir('.')
+    formatted_items = []
+    for item in items:
+        if os.path.isdir(item):
+            formatted_items.append(f"{item} - папка")
+        else:
+            formatted_items.append(f"{item} - файл")
+    msg = '<br>'.join(formatted_items)
+    print(msg)
+    return HttpResponse(msg)
