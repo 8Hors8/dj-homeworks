0a6829d467fc64aeaf916bb67fc141bb8d339349
Hors <iliya.engineer20@gmail.com>
2024-10-17 19:02:50 +0500

    update function handle

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -9,9 +9,21 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
-            phones = list(csv.DictReader(file, delimiter=';'))
+        try:
+            with open('phones.csv', 'r', encoding='utf-8') as file:
+                phones = list(csv.DictReader(file, delimiter=';'))
 
-        for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            for phone_data in phones:
+
+                phone = Phone(
+                    name=phone_data['name'],
+                    price=int(phone_data['price']),
+                    image=phone_data['image'],
+                    release_date=phone_data['release_date'],
+                    lte_exists=phone_data['lte_exists'] == 'True'
+                )
+
+                phone.save()
+                print(f"Телефон {phone.name} успешно сохранён.")
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"Ошибка: {e}"))
