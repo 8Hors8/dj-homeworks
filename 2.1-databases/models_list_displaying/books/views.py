2fc1ddbac0481d0d979c36ad6986bf5d3acb2d05
Hors <iliya.engineer20@gmail.com>
2024-10-17 20:20:13 +0500

    новые вьюхи

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 
+from .models import Book
+
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books = Book.objects.all().order_by('pub_date')
+    context = {
+        'books': books
+    }
+    return render(request, template, context)
+
+
+def books_by_date(request, pub_date):
+    template = 'books/books_list.html'
+
+    try:
+        books = Book.objects.filter(pub_date=pub_date).order_by('pub_date')
+        prev_date = Book.objects.filter(pub_date__lt=pub_date).order_by('-pub_date').first()
+        next_date = Book.objects.filter(pub_date__gt=pub_date).order_by('pub_date').first()
+    except Book.DoesNotExist:
+        raise Http404("Книги за эту дату не найдены.")
+
+    context = {
+        'books': books,
+        'prev_date': prev_date.pub_date if prev_date else None,
+        'next_date': next_date.pub_date if next_date else None
+    }
     return render(request, template, context)
