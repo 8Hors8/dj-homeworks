278ba8b43d4bca6c61b23b5282aa6293fb62396c
Hors <iliya.engineer20@gmail.com>
2024-10-24 18:21:46 +0500

    added models

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,16 +1,40 @@
 from django.db import models
 
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Тег')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-
+    tags = models.ManyToManyField(Tag, through='ArticleScope', related_name='articles')
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
 
     def __str__(self):
         return self.title
+
+
+class ArticleScope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Тег статьи'
+        verbose_name_plural = 'Теги статей'
+
+    def __str__(self):
+        return f'{self.article} - {self.tag}'
